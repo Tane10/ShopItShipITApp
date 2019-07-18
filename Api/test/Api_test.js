0a09210171981d307efd747ccaea062fbeca3ffb
Tane10 <tane2@hotmail.co.uk>
2019-07-18 10:55:11 +0100

    Api Tests

--- Api/test/Api_test.js
+++ Api/test/Api_test.js
@@ -4,21 +4,36 @@ var chai = require("chai"),
 
 chai.use(chaiHttp);
 
-describe("Api Tests", () => {
+describe("Api HTTP async Tests", () => {
   describe("Http calls and requests", () => {
-    it("Once connect to api, you should receivce a status code of 200", () => {
+    it("Once connect to api, you should receivce a status code of 200", done => {
+      chai
+        .request(server)
+        .get("/")
+        .auth("user", "")
+        .end(res => {
+          expect(res).to.have.a.property("code");
+          expect(res).to.have.status(200);
+          expect(res).to.not.have.status(404);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+
+    it("Api should return a 401 if the user doesn't have a correct auth token", done => {
       chai
         .request(server)
         .get("/")
         .end((err, res) => {
           expect(res).to.have.a.property("code");
-          expect(res).to.have.status(200);
-          expect(res).to.not.have.status(404);
+          expect(res).to.have.status(401);
+          done();
+        })
+        .catch(err => {
+          throw err;
         });
     });
-
-    // it("You should be able to send a get request, and retrive JSON back", () => {
-    //   expect(Response).to.equal;
-    // });
   });
 });
