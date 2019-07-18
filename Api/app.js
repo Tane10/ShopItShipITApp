4de4e8e02b4f644eece4ac0b3d653e6c5d54cb65
Tane10 <tane2@hotmail.co.uk>
2019-07-18 19:28:12 +0100

    starting to build out the grapthQL schemas

--- Api/app.js
+++ Api/app.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphQLHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-// bodyParser middle wear for json 
+// bodyParser middle wear for json
 app.use(bodyParser.json());
+
+// setting the grapthql endpoint and bulding schemea
+// query is fetch, mutation is to chage data
+app.use(
+  "/graphql",
+  graphQLHttp({
+    schema: buildSchema(`
+
+    type RootQuery {
+
+    }
+
+    type RootMutation {
+
+    }
+
+    schema : {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    rootValue: {}
+  })
+);
 
 app.listen(3000);
