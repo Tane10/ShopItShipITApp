af87960fe30a8ff1eb0d12efaa2665497046c56b
Tane10 <tane2@hotmail.co.uk>
2019-07-18 07:47:20 +0100

    starting on the the readme

---
+++ Api/MainMenu_test.js
@@ -0,0 +1,10 @@
+var expect = requier('chai').expect; 
+
+
+describe('Main Menu Splash screen', () => {
+    describe('Main menu buttons', () => {
+        it('Each button should beable to clickable', () => {
+            expect(InputFieldClick).to.be.true;
+        });
+    });
+});
